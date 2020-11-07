ed74c5815cd1c28fc955f1aeb6a4a961bc7b9fdb
NiJeTi <nijeti.inc@gmail.com>
2020-11-07 22:36:58 +0300

    Deserializer tests

---
+++ BurpLang.Core.Tests/DeserializationTest.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+using BurpLang.Core.Tests.Entities;
+
+using NUnit.Framework;
+
+namespace BurpLang.Core.Tests
+{
+    public class DeserializationTest
+    {
+        private readonly Entity _entity = new Entity();
+
+        private string? _serializedEntity;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _serializedEntity = File.ReadAllText(Path.Combine("Content", "testSerializedEntity.bl"));
+        }
+
+        [Test]
+        public void Deserialize()
+        {
+            var deserialized = Converter.Deserialize<Entity>(_serializedEntity!);
+
+            Assert.That(deserialized, Is.EqualTo(_entity));
+        }
+    }
+}
