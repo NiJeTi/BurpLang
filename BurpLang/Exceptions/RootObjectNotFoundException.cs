8de19bd844b5108fc78cb6003208cfa7722aa803
NiJeTi <nijeti.inc@gmail.com>
2020-11-20 10:26:12 +0300

    Deserialization exceptions

---
+++ BurpLang/Exceptions/RootObjectNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BurpLang.Exceptions
+{
+    public class RootObjectNotFoundException : Exception
+    {
+        public string Input { get; }
+        
+        public RootObjectNotFoundException(string input) :
+            base($"Root object was not found in:{Environment.NewLine}{input}")
+        {
+            Input = input;
+        }
+    }
+}
---
+++ BurpLang/Exceptions/UnsupportedTypeException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BurpLang.Exceptions
+{
+    public class UnsupportedTypeException : Exception
+    {
+        public UnsupportedTypeException(Type type) : base($"Found unsupported property of type \"{type}\"") { }
+    }
+}
