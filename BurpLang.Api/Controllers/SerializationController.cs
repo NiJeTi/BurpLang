12d476b6d2980046bc56bdcbc5bbc57c9150d270
NiJeTi <nijeti.inc@gmail.com>
2020-11-30 15:30:14 +0300

    Simple controller for deserialization

---
+++ BurpLang.Api/Controllers/SerializationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using BurpLang.Common.Entities;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurpLang.Api.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class SerializationController : ControllerBase
+    {
+        [HttpPost("deserialize")]
+        public IActionResult Deserialize([FromBody] string body)
+        {
+            try
+            {
+                var deserialized = Converter.Deserialize<Entity>(body);
+
+                return Ok(deserialized);
+            }
+            catch (FormatException formatException)
+            {
+                return Ok(formatException.Message);
+            }
+        }
+    }
+}
