8636d1ef9c7b5f52b7ba21a10a57ceb754ddf39c
NiJeTi <nijeti.inc@gmail.com>
2020-11-30 15:28:41 +0300

    Special exception for property parsing related errors

---
+++ BurpLang/Exceptions/PropertyParsingException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BurpLang.Exceptions
+{
+    public class PropertyParsingException : Exception
+    {
+        public PropertyParsingException(string propertyName, string expectedPattern, string actualValue) :
+            base($"Error parsing property \"{propertyName}\". " +
+                $"Expected pattern \"{expectedPattern}\" but was \"{actualValue}\"") { }
+    }
+}
