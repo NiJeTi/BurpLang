e99acdf34ea39f95c1bcc8bb2a06091e07ece748
NiJeTi <nijeti.inc@gmail.com>
2020-12-08 02:08:19 +0300

    Page core

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import './App.css';
+
+import Input from './components/Input';
+import Output from './components/Output';
+
+import initialResponse, {ApiResponse} from './models/apiResponse';
 
 function App() {
+  const [ responseState, setResponseState ] = useState<ApiResponse>(initialResponse);
+
+  const onPost = (data: string) =>
+    postData(data)
+      .then(setResponseState);
+
   return (
-    <h1>Hello world!</h1>
+    <div id="terminal">
+      <Input
+        onValueChange={onPost}
+        error={responseState.error}
+      />
+      <Output
+        response={responseState}
+      />
+    </div>
   );
+}
+
+function postData(data: string): Promise<ApiResponse> {
+  const request: RequestInit = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain'
+    },
+    body: data
+  };
+
+  return fetch('http://localhost:5000/parse', request)
+    .then(response => response.json());
 }
 
 export default App;
