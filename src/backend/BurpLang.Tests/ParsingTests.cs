b9ab8cae718a96cab8f6b7b478de4fbdfb190066
NiJeTi <nijeti.inc@gmail.com>
2020-12-08 01:58:57 +0300

    New test for new entity

--- src/backend/BurpLang.Tests/ParsingTests.cs
+++ src/backend/BurpLang.Tests/ParsingTests.cs
@@ -8,15 +8,52 @@ namespace BurpLang.Tests
 {
     public class ParsingTests
     {
-        private readonly Entity _entity = new Entity();
+        private Entity _expectedEntity = null!;
 
         private Parser<Entity> _parser = null!;
 
         [SetUp]
         public void SetUp()
         {
-            var serializedEntity = File.ReadAllText(Path.Combine("Entities", "SerializedEntity.bl"));
+            _expectedEntity = new Entity
+            {
+                SomeText = "String",
+                ThisIsNumber = 123,
+                ThisIsFloatingPointNumber = 123.456f,
+                SomeLogicalStatement = true,
+                TextLines = new[]
+                {
+                    "String1",
+                    "String2",
+                    "String3"
+                },
+                MultipleNumbers = new[]
+                {
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6
+                },
+                MultipleRealNumbers = new[]
+                {
+                    1.2f,
+                    3.4f,
+                    5.6f,
+                    7.8f,
+                    9.0f
+                },
+                BunchOfStatements = new[]
+                {
+                    true,
+                    false,
+                    false,
+                    true
+                }
+            };
 
+            var serializedEntity = File.ReadAllText(Path.Combine("Entities", "SerializedEntity.bl"));
             _parser = new Parser<Entity>(serializedEntity);
         }
 
@@ -25,7 +62,7 @@ namespace BurpLang.Tests
         {
             var parsedEntity = _parser.GetObject();
 
-            Assert.That(parsedEntity, Is.EqualTo(_entity));
+            Assert.That(parsedEntity, Is.EqualTo(_expectedEntity));
         }
     }
 }
